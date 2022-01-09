29d3927a20cf28f38a8b4acdf31578be47b27150
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-09 18:31:15 -0300

    Testing waves from myself and waves from random people

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,47 @@
+const wavers = [];
+
+const getWaveCount = async (waveContract) => {
+  return await waveContract.getTotalWaves();
+}
+
+const waveAtYourself = async (waveContract) => {
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+}
+
+const getWaveFromARandomPerson = async (waveContract, randomPerson) => {
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  wavers.push(randomPerson.address);
+}
+
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners(); 
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
+  const { log } = console;
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
+  log("Contract deployed to:", waveContract.address);
+  log("Contract deployed by:", owner.address);
 
-  let waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+  await getWaveCount(waveContract);
 
-  waveCount = await waveContract.getTotalWaves();
+  await waveAtYourself(waveContract);
+  log('Oh, you waved at yourself. Here, I\'ll give you a friend and he can wave at you.');
+
+  await getWaveCount(waveContract);
+
+  await getWaveFromARandomPerson(waveContract, randomPerson);
+
+  log(
+    'These are all the people that waved at you (except for yourself):', 
+    wavers.join(', ')
+  );
+
+  await getWaveCount(waveContract);
 };
 
 const runMain = async () => {
